4bc04572ceca7a12db90dc91d296dd265c5299a5
Hidayat <78005878+daeyath@users.noreply.github.com>
2023-11-08 07:02:38 +0700

    Add files via upload

--- sqlite3.cxx
+++ sqlite3.cxx
@@ -4,7 +4,7 @@
 sqlite3 *db;
 char *zErrMsg=0;
 int rc;
-const char nem[]="Not enough memory.\n";
+const char nem[]="Not enough memory!\nTry dividing the SQL into several parts";
 char *sqlstr=NULL;
 
 bool connectdb(const char *filename){
@@ -29,19 +29,19 @@ void exec(const char *str){
 	}else if(nRow>0){
 		int *lenCol=new int[nColumn];
 		for(int i=0; i<nColumn; i++)lenCol[i]=0;
-		int k=-1;
+		int z=-1;
 		for(int i=0; i<nRow+1; i++){
 			for(int j=0; j<nColumn; j++){
-				k++;
-				int l=azResult[k]==NULL?0:strlen(azResult[k]);
+				++z;
+				int l=azResult[z]==NULL?0:strlen(azResult[z]);
 				if(lenCol[j]<l)lenCol[j]=l;
 			}
 		}	
-		k=-1;
+		z=-1;
 		for(int i=0; i<nRow+1; i++){
 			for(int j=0; j<nColumn; j++){
-				k++;
-				printf("%-*s", lenCol[j], azResult[k]);
+				++z;
+				printf("%-*s", lenCol[j], azResult[z]);
 				if(j<nColumn-1)printf(" ");
 			}
 			printf("\n");
@@ -93,21 +93,21 @@ void execfile(const char *fname){
 	FILE *afile=fopen(fname, "r");
 	if(afile !=NULL){
 		const int maxl=128;
-		char str[maxl];
-		int dynlen=0;
-		while(fgets(str, maxl, afile)){
-			dynlen+=strlen(str);
+		char line[maxl];
+		int dyn=0;
+		while(fgets(line, maxl, afile)){
+			dyn+=strlen(line);
 			if(sqlstr==NULL){
-				sqlstr=(char*)malloc(dynlen);
+				sqlstr=(char*)malloc(dyn);
 				sqlstr[0]='\0';
 			}else{
-				sqlstr=(char*)realloc(sqlstr, dynlen);
+				sqlstr=(char*)realloc(sqlstr, dyn);
 			}
 			if(sqlstr==NULL){
 				printf(nem);
 				break;
 			}else{
-				strcat(sqlstr, str);
+				strcat(sqlstr, line);
 			}
 		}
 		fclose(afile);
@@ -120,29 +120,29 @@ void execfile(const char *fname){
 	}
 }
 
-int runinternalcmd(const char *str){
-	int ret=0;
-	const int pos=3;
-	const int len=strlen(str)-pos+1;
-	char param[len];
-	strncpy(param, str+pos, len);
-	bool spc=param[0]==' '||param[strlen(param)-1]==' ';
-	if(!spc){
-		if(strncmp(str,":c ",3)==0){
-			connectdb(param);
-		}else if(strncmp(str, ":e ",3)==0){
-			execfile(param);
-		}else if(strcmp(str,":q")==0){
-			ret=1;
-		}else if(strncmp(str,":s ",3)==0){
-			show(param);
-		}else if(strcmp(str,":h")==0){
-			help();
-		}else{
-			printf("'%s' command not found\n", str);
-		}
+int runinternalcmd(char *str){
+	int ret=0, i=0;
+	char delim[]=" ";
+	char *param[2];
+	for(int i=0; i<2; i++)param[i]=0;
+	char *item=strtok(str, delim);
+	while(item!=NULL){
+		param[i]=item;
+		i++;
+		item=strtok(NULL, delim);
+	}
+	if(strcmp(param[0],":c")==0){
+		connectdb(param[1]);
+	}else if(strcmp(param[0], ":e")==0){
+		execfile(param[1]);
+	}else if(strcmp(param[0],":q")==0){
+		ret=1;
+	}else if(strcmp(param[0],":s")==0){
+		show(param[1]);
+	}else if(strcmp(param[0],":h")==0){
+		help();
 	}else{
-		printf("Don't use space after or first parameter '%s'\n",param);
+		printf("'%s' command not found\n", str);
 	}
 	return ret;
 }
